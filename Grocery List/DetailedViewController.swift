2bf587b3ef612f02f242fef52db2137eb894d758
ChristianHurtado29 <christianhurtado@pursuit.org>
2019-11-21 23:52:44 -0500

    Initial Commit

---
+++ Grocery List/DetailedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailedViewController.swift
+//  Grocery List
+//
+//  Created by Christian Hurtado on 11/21/19.
+//  Copyright © 2019 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class DetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+}
---
+++ Grocery List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grocery List
+//
+//  Created by Christian Hurtado on 11/21/19.
+//  Copyright © 2019 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
