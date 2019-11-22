fe4520ad1066510b0a0a206732498155157493b3
ChristianHurtado29 <christianhurtado@pursuit.org>
2019-11-22 00:22:24 -0500

    configuring cells

---
+++ Grocery List/Items.swift
@@ -0,0 +1,25 @@
+//
+//  Items.swift
+//  Grocery List
+//
+//  Created by Christian Hurtado on 11/22/19.
+//  Copyright © 2019 Christian Hurtado. All rights reserved.
+//
+
+import Foundation
+
+//enum Status: CaseIterable {
+//    case purchased
+//    case notpurchased
+//
+//
+//    static func getData() -> [Status] {
+//      let eventNames = [""]
+//      var events = [Event]()
+//      for eventName in eventNames {
+//        let event = Event(date: Date(), name: eventName)
+//        events.append(event)
+//      }
+//      return events
+//    }
+//}
--- Grocery List/ViewController.swift
+++ Grocery List/ViewController.swift
@@ -14,12 +14,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     
+    var items:String = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.dataSource = self
     }
 
 
 }
 
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "leftCell", for: indexPath)
+        
+//        let item
+//        cell.textLabel
+        
+        return cell
+    }
+    
+    
+}
